9e4d950707963a0cc8e5aa1c7547e9518b2277df
sirikwan <k.sirikwann@gmail.com>
2016-04-26 11:23:00 +0700

    ออกแบบพื้นหลัง

--- app/src/main/java/ice_pbru/khamhor/sirikwan/ice_database/MainActivity.java
+++ app/src/main/java/ice_pbru/khamhor/sirikwan/ice_database/MainActivity.java
@@ -1,13 +1,30 @@
 package ice_pbru.khamhor.sirikwan.ice_database;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bind widget
+        bindwidget();
+
+
+    }//Main Method
+    public void clickRegis(View view) {
+        startActivity(new Intent(MainActivity.this,SignUp.class));
     }
-}
+
+    private void bindwidget() {
+
+    }
+
+
+}//Main class
