c249ea99b6ab122cc4f2aa7001ef1269f625d989
sirikwan <k.sirikwann@gmail.com>
2016-04-27 15:40:28 +0700

    หน้า log in

--- app/src/main/java/ice_pbru/khamhor/sirikwan/ice_database/MainActivity.java
+++ app/src/main/java/ice_pbru/khamhor/sirikwan/ice_database/MainActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
 
     }//Main Method
+
+    public void clickIn(View view) {
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (checkSpace()) {
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,"มีช่องว่าง","กรุณาตรวจสอบข้อมูล");
+
+        }
+    }
+
+    private boolean checkSpace() {
+        return
+        userString.equals("") ||
+                passwordString.equals("");
+    }
+
     public void clickRegis(View view) {
         startActivity(new Intent(MainActivity.this, SignUp.class));
     }
@@ -28,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void bindwidget() {
+        userEditText = (EditText) findViewById(R.id.editText6);
+        passwordEditText = (EditText) findViewById(R.id.editText7);
 
     }
 
