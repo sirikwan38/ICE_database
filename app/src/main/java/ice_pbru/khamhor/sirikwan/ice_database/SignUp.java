118a0b0416715bb4d627f3c10c5688c9eaf44d5e
sirikwan <k.sirikwann@gmail.com>
2016-04-27 14:52:35 +0700

    หน้า sign in

--- app/src/main/java/ice_pbru/khamhor/sirikwan/ice_database/SignUp.java
+++ app/src/main/java/ice_pbru/khamhor/sirikwan/ice_database/SignUp.java
@@ -2,12 +2,67 @@ package ice_pbru.khamhor.sirikwan.ice_database;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, surnameEditText, userEditText, passwordEditText , emailEditText;
+    private String nameString, surnameString, userString, passwordString, emailString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //bindWidget
+        bindWidget();
+
+
+
+
+
+    }//Main Method
+
+    public void clickSignUp (View view) {
+        nameString = nameEditText.getText().toString().trim();
+        surnameString = surnameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = emailEditText.getText().toString().trim();
+        emailString = emailEditText.getText().toString().trim();
+
+        if (checkSpace()) {
+            //Have space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,"มีช่องว่าง","กรุณาตรวจสอบข้อมูลและกรอกให้ครบถ้วน");
+        } else {
+
+        }
+
+
+    }//clickSignUp
+
+    private boolean checkSpace() {
+
+        return nameString.equals("")||
+                surnameString.equals("")||
+                userString.equals("")||
+                passwordString.equals("")||
+                emailString.equals("");
+
     }
-}
+
+    private void bindWidget() {
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnameEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+        emailEditText = (EditText) findViewById(R.id.editText5);
+
+
+    }//bind Widget
+
+
+}//Main class
