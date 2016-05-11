8bd7394b01b2d9943fa9d6ebc43a41349203f683
sirikwan <k.sirikwann@gmail.com>
2016-05-11 16:09:25 +0700

    :)

--- app/src/main/java/ice_pbru/khamhor/sirikwan/ice_database/LoginSuscess.java
+++ app/src/main/java/ice_pbru/khamhor/sirikwan/ice_database/LoginSuscess.java
@@ -1,9 +1,13 @@
 package ice_pbru.khamhor.sirikwan.ice_database;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.squareup.okhttp.OkHttpClient;
@@ -26,38 +30,12 @@ public class LoginSuscess extends AppCompatActivity {
         SynJSON synJSON = new SynJSON();
         synJSON.execute();
 
-    }//Main method
+    }//1
 
     public class SynJSON extends AsyncTask<Void, Void, String> {
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            try {
-                Log.d("SIRIKWAN", "Response ==> " + s);
-                JSONArray jsonArray = new JSONArray(s);
-
-                String[] iconStrings = new String[jsonArray.length()];
-                String[] titleStrings = new String[jsonArray.length()];
-                String[] eBookStrings = new String[jsonArray.length()];
-
-                for (int i=0; i < jsonArray.length();i++ ) {
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                    iconStrings[i] = jsonObject.getString("Cover");
-                    titleStrings[i] = jsonObject.getString("Name");
-                    eBookStrings[i] = jsonObject.getString("Ebook");
-                }//for
-
-                ProductAdapter productAdapter = new ProductAdapter(LoginSuscess.this, iconStrings, titleStrings);
-                listView.setAdapter(productAdapter);
-
-            } catch (Exception e) {
-                Log.d("SIRIKWAN", "onPost -->" + e.toString());
-            }
-        }
-
-        @Override
-        protected String doInBackground(Void... voids) {
+        protected String doInBackground(Void... voi) {
             try {
                 String strURL = "http://ice.pbru.ac.th/ICE56/sirikwan/php_get_foodtable1.php";
                 OkHttpClient okHttpClient = new OkHttpClient();
@@ -72,6 +50,43 @@ public class LoginSuscess extends AppCompatActivity {
             }
 
         }//doInback
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                Log.d("SIRIKWAN", "Response ==> " + s);
+                JSONArray jsonArray = new JSONArray(s);
+
+                final String[] iconStrings = new String[jsonArray.length()];
+                String[] titleStrings = new String[jsonArray.length()];
+                final String[] eBookStrings = new String[jsonArray.length()];
+
+                for (int i=0; i < jsonArray.length();i++ ) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    iconStrings[i] = jsonObject.getString("Cover");
+                    titleStrings[i] = jsonObject.getString("Name");
+                    eBookStrings[i] = jsonObject.getString("Ebook");
+                }//for
+
+                ProductAdapter productAdapter = new ProductAdapter(LoginSuscess.this, iconStrings, titleStrings);
+                listView.setAdapter(productAdapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(eBookStrings[i]));
+                        startActivity(intent);
+
+                    }
+                });
+
+            } catch (Exception e) {
+                Log.d("SIRIKWAN", "onPost -->" + e.toString());
+            }
+        }
+
+
     }
 
 }//Main Class
